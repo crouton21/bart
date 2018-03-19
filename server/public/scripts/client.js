b149359c6b8665822823dc920e73722ba379b278
Selena Carlson <selena.h.carlson@gmail.com>
2018-03-19 16:15:23 -0500

    inputs ugly but incorporated

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngTagsInput']);
 
 /// Routes ///
 myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
